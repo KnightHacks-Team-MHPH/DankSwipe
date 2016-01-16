faf767e830d8eabc6642639813aeaf98bf6782a0
ThatITNinja <ThatITNinja@hotmail.com>
2016-01-16 12:30:35 -0500

    first pass at allowing to sell the dankest of memes

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -5,14 +5,14 @@ class Meme < ActiveRecord::Base
   
   def sell
     # find the user that invested the most
-    highest_investment = @meme.investments.order("amount ASC").first
-    investment_owner = highest_investment.first.user
+    highest_investment = self.investments.order("amount ASC").first
+    investment_owner = highest_investment.user
     # assign the meme to the new owner
-    @meme.owner_id = investment_owner.id
-    @meme.save!
+    self.owner_id = investment_owner.id
+    self.save!
     
     # disperse investments once again
-    @meme.investments.each do |investment|
+    self.investments.each do |investment|
       investment_user = investment.user
       # give the user back its currency
       investment_user.currency += investment.amount.to_i
@@ -22,12 +22,12 @@ class Meme < ActiveRecord::Base
     end
     
     # give the user the currency for each collected right swipe minus the left swipes
-    right_swipes = @meme.swipes.where(direction: :right, collected: false).count
-    left_swipes = @meme.swipes.where(direction: :left, collected: false).count
-    @meme.user.currency += right_swipes - left_swipes
+    right_swipes = self.swipes.where(direction: :right, collected: false).count
+    left_swipes = self.swipes.where(direction: :left, collected: false).count
+    self.user.currency += right_swipes - left_swipes
     
     # we have used them, set them to collected so they wont be recounted in future sales
-    @meme.swipes.each do |swipe|
+    self.swipes.each do |swipe|
       swipe.collected = true
       swipe.save!
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   
   resources :memes
+  get '/acquired_memes' => "memes#acquired_memes", as: :acquired_memes
+  post '/sell/:id' => "memes#sell", as: :sell_meme
   
   get '/swipe' => "memes#swipe_index"
   post '/swipe/:id/left' => "memes#swipe_left", as: :left_swipe
